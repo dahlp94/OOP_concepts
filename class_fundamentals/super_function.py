83d19523f5a8aea8b3ee79e64f6300d10ec06f9d
Pratik Dahal <mapratikdahal@gmail.com>
2024-08-02 17:14:54 -0500

    added some methods

--- class_fundamentals/super_function.py
+++ class_fundamentals/super_function.py
@@ -6,11 +6,20 @@ class Square(Rectangle):
         self.length = length
         self.width = width
 
+    def area(self):
+        return self.length * self.width
+
 class Cube(Rectangle):
     def __init__(self, length, width, height):
         self.length = length
         self.width = width
         self.height = height
 
+    def volume(self):
+        return self.length * self.width * self.length
+
 square = Square(3,3)
 cube = Cube(3,3,3)
+
+print(square.area())
+print(cube.volume())
